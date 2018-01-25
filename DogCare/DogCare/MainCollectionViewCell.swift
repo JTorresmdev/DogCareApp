cecb08f8edfbb9f6f408f8088236fa5fd0072732
Justin Torres <touchofsarcasm@gmail.com>
2018-01-25 05:14:01 -0500

    added cocoapods and made progress

---
+++ DogCare/DogCare/MainCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MainCollectionViewCell.swift
+//  DogCare
+//
+//  Created by Justin Torres on 1/25/18.
+//  Copyright © 2018 Justin Torres. All rights reserved.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var container: UIView!
+    
+}
--- DogCare/DogCare/MainDogViewController.swift
+++ DogCare/DogCare/MainDogViewController.swift
@@ -1,35 +1,46 @@
-//
-//  MainDogViewController.swift
-//  DogCare
-//
-//  Created by Justin Torres on 1/24/18.
-//  Copyright © 2018 Justin Torres. All rights reserved.
-//
-
 import UIKit
+import UICollectionViewGallery
 
-class MainDogViewController: UIViewController {
+//reuseidentifier for cell to prevent from re-typing
+let reuseCell = "CollectionReuse"
 
+class MainDogViewController: UIViewController{
+
+    
+    //collection view that will hold the dog info
+    @IBOutlet weak var mainCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //using cocoapods UICollectionViewGllery to customize uicollectionview design
+        mainCollectionView.setGallery(withStyle: .horizontal, minLineSpacing: 20, itemSize: CGSize(width: 300, height: 300), minScaleFactor: 0.8)
+        //assigning collectionview cell nib (Custom cell)
+        mainCollectionView.register(UINib(nibName: "CustomCell", bundle: nil), forCellWithReuseIdentifier: reuseCell)
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+//Extension for collection view related things
+extension MainDogViewController: UICollectionViewDataSource, UICollectionViewDelegate{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseCell, for: indexPath) as! MainCollectionViewCell
+        cell.container.layer.cornerRadius = 20
+        return cell
+        
+    }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        mainCollectionView.recenterIfNeeded()
+    }
+    
+    
+    
 }
