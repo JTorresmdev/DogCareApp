8755243174a36559bd99beda0a89a29848e6a8be
Justin Torres <touchofsarcasm@gmail.com>
2018-02-13 00:34:59 -0500

    Fixed collection and added about us page plus nagivation bar

--- DogCare/DogCare/MainDogViewController.swift
+++ DogCare/DogCare/MainDogViewController.swift
@@ -12,11 +12,33 @@ class MainDogViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //using cocoapods UICollectionViewGllery to customize uicollectionview design
-        mainCollectionView.setGallery(withStyle: .horizontal, minLineSpacing: 20, itemSize: CGSize(width: 300, height: 300), minScaleFactor: 0.8)
+        //Adjusting collectionView Style
+        adjustmentsForStyle()
         //assigning collectionview cell nib (Custom cell)
         mainCollectionView.register(UINib(nibName: "CustomCell", bundle: nil), forCellWithReuseIdentifier: reuseCell)
     }
+    
+    
+    func adjustmentsForStyle(){
+        switch UIScreen.main.nativeBounds.height {
+        case 2436:
+            //iphone X
+            //using cocoapods UICollectionViewGllery to customize uicollectionview design
+            let width = UIScreen.main.nativeBounds.width
+            let height = UIScreen.main.nativeBounds.height
+            let styleSize = CGSize(width: width/3.2, height: height/6)
+            //iPhone X condition
+            mainCollectionView.setGallery(withStyle: .horizontal, minLineSpacing: -40, itemSize: styleSize, minScaleFactor: 0.8)
+        default:
+            //using cocoapods UICollectionViewGllery to customize uicollectionview design
+            let width = UIScreen.main.nativeBounds.width
+            let height = UIScreen.main.nativeBounds.height
+            let styleSize = CGSize(width: width/3.2, height: height/5)
+            //iPhone X condition
+            mainCollectionView.setGallery(withStyle: .horizontal, minLineSpacing: -40, itemSize: styleSize, minScaleFactor: 0.8)
+        }
+    }
+    
 }
 
 
